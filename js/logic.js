e74198fd5db444b6603019f1014cc3aabd381a2c
tito <tito@cefalo.com>
2016-09-21 15:45:24 +0600

    Google calendar API to fetch event data

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,39 @@
 window.onload =function(){
-    new Logic();
-}
+  
+    var cy = cytoscape({
+	  // very commonly used options
+	  container: document.getElementById('cy'),
+	  layout: { name: 'grid' /* , ... */ },
 
-function Logic(){
-    var date = new Date
+	  // initial viewport state:
+	  zoom: 1,
+	  pan: { x: 0, y: 0 },
+
+	  // interaction options:
+	  minZoom: 1e-50,
+	  maxZoom: 1e50,
+	  zoomingEnabled: true,
+	  userZoomingEnabled: true,
+	  panningEnabled: true,
+	  userPanningEnabled: true,
+	  boxSelectionEnabled: false,
+	  selectionType: 'single',
+	  touchTapThreshold: 8,
+	  desktopTapThreshold: 4,
+	  autolock: false,
+	  autoungrabify: false,
+	  autounselectify: false,
+
+	  // rendering options:
+	  headless: false,
+	  styleEnabled: true,
+	  hideEdgesOnViewport: false,
+	  hideLabelsOnViewport: false,
+	  textureOnViewport: false,
+	  motionBlur: false,
+	  motionBlurOpacity: 0.2,
+	  wheelSensitivity: 1,
+	  pixelRatio: 'auto'
+	});
+
 }
