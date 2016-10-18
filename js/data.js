e8075947fbcfe50ca95a60d5d64c17af73060dac
tito <tito@cefalo.com>
2016-10-18 10:51:37 +0600

    data fetching added

---
+++ js/data.js
@@ -0,0 +1,76 @@
+var DataFetcher=function(argument) {
+	this._DataCallabk=false;
+
+	this.addGoogleAPI=function(callBack){
+		this._DataCallabk=callBack;
+
+	 	var iDiv = document.createElement('div');
+		iDiv.id = 'authorize-div';
+		document.getElementsByTagName('body')[0].appendChild(iDiv);
+
+		var span = document.createElement("span");
+		var node = document.createTextNode("Authorize access to Google Calendar API");
+		span.appendChild(node);
+		iDiv.appendChild(span);
+
+		var btn = document.createElement("button");        
+		btn.appendChild(document.createTextNode("Authorize"));                               
+		iDiv.appendChild(btn); 
+		btn.addEventListener("click", this.glAuthorizeMe.bind(this), true);
+		//script
+		var script = document.createElement('script');
+		script.src = "https://apis.google.com/js/client.js";
+		document.getElementsByTagName('body')[0].appendChild(script);
+ 	}
+ 	this.glAuthorizeMe=function() {
+ 		var self=this;
+	    gapi.auth.authorize({
+	        'client_id': CLIENT_ID,
+	        'scope': "https://www.googleapis.com/auth/calendar.readonly",
+	        'immediate': true
+	    }, function(authResult) {
+	          var authorizeDiv = document.getElementById('authorize-div');
+	          if (authResult && !authResult.error) {
+	            authorizeDiv.style.display = 'none';
+	            gapi.client.load('calendar', 'v3', self._getAllEvents.bind(self));
+	          } else {
+	            authorizeDiv.appendChild(document.createElement('br'));
+	            authorizeDiv.appendChild(document.createTextNode("Error:"+authResult.error+","+authResult.error_subtype));
+	          }
+	        }
+	    );
+  	}
+  	this._getAllEvents=function() {
+	    var self=this,
+	    	date_from=new Date("01 Jan 2014"),
+	        date_to=new Date();
+	        date_to.setDate(date_to.getDate() + 1); //till tomorrow?
+
+	    gapi.client.calendar.events.list({
+	      'calendarId': 'primary',
+	      'timeMin': date_from.toISOString(),'timeMax': date_to.toISOString(),
+	      'showDeleted': false, 'singleEvents': true,
+	      'maxResults': 999999,'orderBy': 'startTime'
+	    }).execute(function(resp) {
+	      self.setData(resp.items);
+	      if(self._DataCallabk)
+	      	self._DataCallabk(resp.items);
+
+	    });
+	};
+	this.getData=function(){
+	    if (typeof(Storage) !== "undefined") {
+	        var data=localStorage.getItem("events");
+	        if(data){
+	        	return JSON.parse(data);
+	        }
+	    }
+	    return false;
+	}
+
+	this.setData=function(data){
+	 	if (typeof(Storage) !== "undefined") {
+	        localStorage.setItem("events", JSON.stringify(data));
+	    }
+	 }
+}
--- js/logic.js
+++ js/logic.js
@@ -1,10 +1,11 @@
 window.onload =function(){
-   var data=getData();
-   if(!data){
-   		addGoogleAPI();
-   }else{
-   		processData(data);
-   }
+  var datafetcher=new DataFetcher();
+  var data=datafetcher.getData();
+  if(data){
+      processData(data);
+  }else{
+    datafetcher.addGoogleAPI(processData);
+  }
 }
 
 function processData(data){
@@ -17,72 +18,8 @@ function processData(data){
     }
     document.close();
 }
-function getData(){
-    if (typeof(Storage) !== "undefined") {
-        var data=localStorage.getItem("events");
-        if(data){
-        	return JSON.parse(data);
-        }
-    }
-    return false;
- }
 
- function addGoogleAPI(){
 
- 	var iDiv = document.createElement('div');
-	iDiv.id = 'authorize-div';
-	document.getElementsByTagName('body')[0].appendChild(iDiv);
+ 
+ 
 
-	var span = document.createElement("span");
-	var node = document.createTextNode("Authorize access to Google Calendar API");
-	span.appendChild(node);
-	iDiv.appendChild(span);
-
-	var btn = document.createElement("button");        
-	btn.appendChild(document.createTextNode("Authorize"));                               
-	iDiv.appendChild(btn); 
-	btn.onClick=glAuthorizeMe;
-	//script
-	var script = document.createElement('script');
-	script.src = "https://apis.google.com/js/client.js?onload=glAuthorizeMe";
-	document.getElementsByTagName('body')[0].appendChild(script);
- }
- function glAuthorizeMe() {
-    gapi.auth.authorize({
-        'client_id': CLIENT_ID,
-        'scope': SCOPES.join(' '),
-        'immediate': true
-      }, function(authResult) {
-          var authorizeDiv = document.getElementById('authorize-div');
-          if (authResult && !authResult.error) {
-            // Hide auth UI, then load client library.
-            authorizeDiv.style.display = 'none';
-            gapi.client.load('calendar', 'v3', _getAllEvents);
-          } else {
-            authorizeDiv.appendChild(document.createElement('br'));
-            authorizeDiv.appendChild(document.createTextNode("Error:"+authResult.error+","+authResult.error_subtype));
-          }
-        }
-    );
-  }
-function _getAllEvents() {
-    var date_from=new Date("01 Jan 2014"),
-        date_to=new Date();
-        date_to.setDate(date_to.getDate() + 1); //till tomorrow?
-
-    gapi.client.calendar.events.list({
-      'calendarId': 'primary',
-      'timeMin': date_from.toISOString(),'timeMax': date_to.toISOString(),
-      'showDeleted': false, 'singleEvents': true,
-      'maxResults': 999999,'orderBy': 'startTime'
-    }).execute(function(resp) {
-      setData(resp.items);
-      processData(resp.items);
-
-    });
-}
-function setData(data){
- 	if (typeof(Storage) !== "undefined") {
-        localStorage.setItem("events", JSON.stringify(data));
-    }
- }
