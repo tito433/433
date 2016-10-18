4cedd5bd18d5556428db72a69789c7bc35a2aea1
tito <tito@cefalo.com>
2016-10-18 13:08:45 +0600

    style

--- js/logic.js
+++ js/logic.js
@@ -9,14 +9,20 @@ window.onload =function(){
 }
 
 function processData(data){
-	document.open("text/html", "replace");
+	// document.open("text/html", "replace");
 	if (data.length > 0) {
-		console.log(data)
-        document.write(JSON.stringify(data));
+      var options = {
+        valueNames: [ 'summary', 'dateTime' ],
+        item: '<li><h3 class="summary"></h3><p class="dateTime"></p></li>'
+      };
+		  var userList = new List('list', options, data);
+      userList.on('searchComplete',function(list){
+        console.log(list.items);
+      })
     } else {
-        document.write("No events found!");
+      document.write("No events found!");
     }
-    document.close();
+  // document.close();
 }
 
 
