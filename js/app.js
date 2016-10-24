098ef58bc419094df7b58cf563fed1de6bfa0a8d
tito <tito@cefalo.com>
2016-10-24 11:16:42 +0600

    Centered label

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
+"use strict";
+
 function Application(){
   Canvas.call(this,document.getElementById('output'));
   this.processData=function(data){
     if (data.length > 0) {
         var option={
-          filter:this.output.bind(this),
+          filter:this.display.bind(this),
           childTemplate:'<li><h3 class="summary">{summary}</h3><p class="dateTime">{start.dateTime}</p></li>'
         };
 
         new List(document.getElementById('list'),option)
                 .draw(data,[ 'summary', 'start.dateTime' ]);
         //draw now
-        this.output(data);
+        this.display(data);
       }
   }
-  this.output=function(data){
+  this.display=function(data){
     if(data && data.length){
-      console.log(data.length);
-      var w=h=50,x=0,y=0;
-      this.drawables.length=0;
-
+      var w=50,h=50,x=0,y=0;
+      this.drawables=[];
       for(var i=0,ln=data.length;i<ln;i++){
         var evt=data[i];
         this.addDrawable(new CanvasRect(evt.summary).size(w,h).position(x,y));
@@ -28,7 +28,6 @@ function Application(){
         }
         
       }
-      console.log(this)
       this.draw();
       
     }
@@ -48,12 +47,54 @@ function CanvasRect(label){
   this.label=label||"";
 
   this.draw=function(ctx){
+    ctx.save();
+    Drawable.prototype.draw.call(this,ctx);
+
     ctx.beginPath();
     ctx.strokeRect(this.x,this.y,this.w,this.h);
-
-    ctx.textAlign="center"; 
-    ctx.fillText(this.label,this.x+this.w/2,this.y+this.h/2);
+    if(""!=this.label){
+      this._centerLabel(ctx);
+    }
+    ctx.restore();
+  }
+  this._centerLabel=function(ctx){
     
+    var lines = function(ctx) {
+        // We give a little "padding"
+        var mw = this.w - 5;
+        ctx.font = this.fontSize+'px '+this.fontName;
+        var words = this.label.split(' ');
+        var new_line = words[0];
+        var lines = [];
+        for(var i = 1; i < words.length; ++i) {
+          var size=ctx.measureText(new_line + " " + words[i]);
+           if (size.width< mw) {
+               new_line += " " + words[i];
+           } else {
+               lines.push(new_line);
+               new_line = words[i];
+           }
+        }
+        lines.push(new_line);
+        return lines;
+    }.call(this,ctx);
+
+    var width = ctx.measureText(lines.reduce(function(p,c){
+                  return p.length>c.length?p:c;})).width,
+        eh=ctx.measureText('M').width,
+        height=eh*(lines.length+1);
+
+    if (width>=this.w || height>= this.h) {
+        this.fontSize-=1;
+        this._centerLabel(ctx);
+    } else {
+        var ly =  this.y+(this.h - height)/2,
+            lx = 0;
+        for (var j = 0; j < lines.length; ++j, ly+=eh) {
+            lx = this.x+this.w/2-ctx.measureText(lines[j]).width/2;
+            ctx.fillText(lines[j], lx, ly);
+        }
+    }
   }
 }
 CanvasRect.prototype = Object.create(Drawable.prototype);
