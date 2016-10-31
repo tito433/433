bc05c8e5372f57c27e371510720336923cee8ac6
tito <tito@cefalo.com>
2016-10-31 09:41:55 +0600

    refactored

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function Application(){
   Canvas.call(this,document.getElementById('output'));
   this.dataFetcher=new DataFetcher();
   this.dataKeyset=[ 'summary', 'start.dateTime' ];
-  
+  this.layout=new Layout(this.width,this.height);
 
   this.processData=function(data){
     if (data.length > 0) {
@@ -23,18 +23,26 @@ function Application(){
         if(keyA > keyB) return 1;
         return 0;
       });
-      var w=((this.width-70)/7),h=w,x=0,y=0;
+
       this.drawables=[];
+      
       for(var i=0,ln=data.length;i<ln;i++){
-        var evt=data[i];
-        this.addDrawable(new CanvasDate(evt.start.dateTime).size(w,h).position(x,y));
-        x+=w+10;
-        if(x>this.width||x+w>this.width){
-          x=0;y+=h+10;
-        }
+        var date=new Date(data[i].start.dateTime),
+            year=date.getFullYear(),
+            month=date.getMonth(),
+            day=date.getDate(),
+            hour=date.getHours(),
+            mins=date.getMinutes();
+
+        var rect=new Rectangle().size(500,500);
+            rect.label=[month,day,hour+':'+mins];
+            rect.fontSize=18;
+
+        this.drawables.push(rect);
       }
+
+      this.layout.flowLeft(this.drawables);
       this.draw();
-      console.log(data);
     }
   }
 
@@ -46,32 +54,3 @@ function Application(){
   
 
 }
- 
- 
-
-function CanvasDate(strDate){
-  Drawable.call(this);
-  this.w=50;
-  this.h=50;
-  this.fontSize=18;
-  var date=new Date(strDate),
-      year=date.getFullYear(),
-      month=date.getMonth(),
-      day=date.getDate(),
-      hour=date.getHours(),
-      mins=date.getMinutes();
-
-  this.label=[month,day,hour+':'+mins];
-
-  this.draw=function(ctx){
-    ctx.save();
-    Drawable.prototype.draw.call(this,ctx);
-
-    ctx.beginPath();
-    ctx.strokeRect(this.x,this.y,this.w,this.h);
-    ctx.restore();
-  }
-  
-}
-CanvasDate.prototype = Object.create(Drawable.prototype);
-CanvasDate.prototype.constructor = CanvasDate;
