349e3c256da364750ee18008bc5acf70dddff992
tito <tito@cefalo.com>
2017-02-08 14:07:16 +0600

    cycloid plugin drawn

--- js/plugins/cycloid.js
+++ js/plugins/cycloid.js
@@ -4,48 +4,30 @@ function Cycloid() {
 	Canvas.call(this, this.output);
 
 	this.addView();
-	/* TODO: make rows and cols configurable */
-	var inpRadious = this.addModel('Cycloid Radius', {
-		'type': 'number',
-		'value': 43,
-		'input.group': 'input-group',
-		'input.class': 'form-control'
-	});
 
 	this.view = function() {
 		this.clear();
+		var centerX = this.width / 2,
+			centerY = this.height / 2;
+		var maxR = this.height / 2;
 
 		var data = this.data;
 		if (!this.data) return false;
+		for (var i = 0, ln = this.data.length; i < ln; i++) {
+			var date = new Date(this.data[i].start.dateTime);
+			var day = date.getDate(),
+				hour = date.getHours();
+			console.log(day, hour)
+			var layer = new PeripheralCircle(centerX, centerY, day.mapTo(1, 31, 0, maxR));
+			layer.size = hour;
 
-		var centerX = this.width / 2,
-			centerY = this.height / 2,
-			radius = parseInt(inpRadious.value);
 
-		for (var i in data) {
-			var date = new Date(data[i].start.dateTime),
-				hour = date.getHours(),
-				hour = hour.mapTo(0, 23, 0, 4);
-
-			var circle = new PeripheralCircle(centerX, centerY, radius, hour);
-			this.add(circle);
-			radius += hour;
-
+			this.add(layer);
 		}
+
+
 		this.draw();
 	}
-	var self = this,
-		_view = this.view,
-		_rInterval = false;
-
-	inpRadious.addEventListener("mousedown", function() {
-		_rInterval = setInterval(function() {
-			_view.call(self);
-		}, 1000 / 30);
-	});
-	inpRadious.addEventListener("mouseup", function() {
-		clearInterval(_rInterval);
-	});
 
 	if (this._isView()) this.view();
 }
@@ -57,15 +39,41 @@ function PeripheralCircle(x, y, r) {
 	this.x = x;
 	this.y = y;
 	this.r = r;
+	this.size = 12;
 
 	this.draw = function(ctx) {
 		ctx.save();
 		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-		ctx.strokeStyle = '#444';
+		ctx.lineWidth = 0.5;
+		// ctx.strokeStyle = '#444';
+		// ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+		// ctx.stroke();
+
+		var sr = this.r / this.size;
+		ctx.beginPath();
+		ctx.strokeStyle = '#333';
+
+
+		var theta = 0,
+			rad = Math.PI / 180;
+
+
+		while (theta <= 360) {
+			var deg = rad * theta;
+			var k = this.size + 1;
+			var x = sr * k * Math.cos(deg) - sr * Math.cos(k * deg),
+				y = sr * k * Math.sin(deg) - sr * Math.sin(k * deg);
+			if (theta == 0) {
+				ctx.moveTo(this.x + x, this.y + y);
+			} else {
+				ctx.lineTo(this.x + x, this.y + y);
+			}
+			theta++;
+		}
 		ctx.stroke();
 		ctx.restore();
 	}
+
 }
 
 PeripheralCircle.prototype = Object.create(Drawable.prototype);
