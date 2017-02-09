47b1011f5d5b8d497d5419998d24c84e1c41db47
tito <tito@cefalo.com>
2017-02-09 11:48:18 +0600

    Cycloid get colors!

--- js/plugins/cycloid.js
+++ js/plugins/cycloid.js
@@ -3,6 +3,9 @@ function Cycloid() {
 	//adapt drawing
 	Canvas.call(this, this.output);
 
+	var self = this;
+
+
 	this.addView();
 
 	this.view = function() {
@@ -17,15 +20,10 @@ function Cycloid() {
 			var date = new Date(this.data[i].start.dateTime);
 			var day = date.getDate(),
 				hour = date.getHours();
-			console.log(day, hour)
 			var layer = new PeripheralCircle(centerX, centerY, day.mapTo(1, 31, 0, maxR));
 			layer.size = hour;
-
-
 			this.add(layer);
 		}
-
-
 		this.draw();
 	}
 
@@ -41,26 +39,34 @@ function PeripheralCircle(x, y, r) {
 	this.r = r;
 	this.size = 12;
 
+	var getRandom = function(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 	this.draw = function(ctx) {
 		ctx.save();
 		ctx.beginPath();
 		ctx.lineWidth = 0.5;
-		// ctx.strokeStyle = '#444';
+		var spec = Math.round(this.size.mapTo(0, 23, 430, 740));
+
+		var clrIndx = Math.round(spec.mapTo(430, 740, 255, 16711680));
+		var clr = '#' + clrIndx.toString(16)
+		// ctx.strokeStyle = clr;
 		// ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
 		// ctx.stroke();
 
-		var sr = this.r / this.size;
+		var sr = this.r / this.size,
+			k = this.size + 1;
 		ctx.beginPath();
-		ctx.strokeStyle = '#333';
+		ctx.strokeStyle = clr;
 
 
 		var theta = 0,
 			rad = Math.PI / 180;
 
-
 		while (theta <= 360) {
 			var deg = rad * theta;
-			var k = this.size + 1;
 			var x = sr * k * Math.cos(deg) - sr * Math.cos(k * deg),
 				y = sr * k * Math.sin(deg) - sr * Math.sin(k * deg);
 			if (theta == 0) {
