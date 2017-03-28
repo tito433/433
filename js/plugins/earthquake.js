d61550c6095edc2275ec8d1d9b58515adcc0b63f
tito <tito@cefalo.com>
2017-03-28 10:09:34 +0600

    3 doors to choose from

--- js/plugins/earthquake.js
+++ js/plugins/earthquake.js
@@ -3,15 +3,13 @@ function Earthquake(input, output) {
 	Canvas.call(this, output);
 
 	var distance = this.height / 2 - 100;
-	var dataImage = false;
-	var yRotate = 0;
-	var xRotate = 0;
+	var rotation = new Point3D();
+
 
 	this.view = function(param) {
 		this.clear();
-		var width = this.width;
-		var height = this.height;
 		var earth = new Sphere3D(Math.abs(distance));
+		earth.rotation(rotation).size(this.width, this.height);
 		this.add(earth);
 		this.draw();
 	}
@@ -23,8 +21,8 @@ function Earthquake(input, output) {
 	}
 	this.onDrag = function(dx, dy) {
 		if (this._isView()) {
-			yRotate += dy / 10;
-			xRotate += dx / 10;
+			rotation.y -= dx / 1000;
+			rotation.x -= dy / 1000;
 			this.view();
 		}
 
@@ -39,27 +37,24 @@ function Earthquake(input, output) {
 
 
 function Point3D(x, y, z) {
-	this.x = isNaN(x) ? 0 : x;
-	this.y = isNaN(y) ? 0 : y;
-	this.z = isNaN(z) ? 0 : z;
-	console.log(this)
+	this.x = isNaN(Number(x)) ? 0 : x;
+	this.y = isNaN(Number(y)) ? 0 : y;
+	this.z = isNaN(Number(z)) ? 0 : z;
 }
 
 function Sphere3D(radius) {
 	Drawable.call(this);
 
 	this.vertices = new Array();
-	this.radius = (typeof(radius) == "undefined" || typeof(radius) != "number") ? 20.0 : radius;
-	this.rings = 16;
-	this.slices = 32;
-	this.numberOfVertices = 0;
+	this.radius = radius;
+	this.rings = 18;
+	this.slices = 18;
+
 
 	var M_PI_2 = Math.PI / 2;
 	var dTheta = (Math.PI * 2) / this.slices;
 	var dPhi = Math.PI / this.rings;
-	var width = radius * 2;
-	var height = radius * 2;
-	var rotation = new Point3D();
+	this._rotation = new Point3D();
 	var lastX = -1;
 	var lastY = -1;
 	// Iterate over latitudes (rings)
@@ -73,27 +68,40 @@ function Sphere3D(radius) {
 			var theta = lon * dTheta;
 			var cosTheta = Math.cos(theta);
 			var sinTheta = Math.sin(theta);
-			this.vertices[this.numberOfVertices] = new Point3D(this.radius * cosTheta * cosPhi,
-				this.radius * sinPhi,
+			var point = new Point3D(this.radius * cosTheta * cosPhi, this.radius * sinPhi,
 				this.radius * sinTheta * cosPhi);
+			this.vertices.push(point);
 
-			this.numberOfVertices++;
 		}
 	}
 	this.rotation = function(dx, dy) {
-		rotation.x += dx;
-		rotation.y += dy;
+		if (dx != undefined) {
+			if (dx instanceof Point3D) {
+				this._rotation = dx;
+			} else {
+				this._rotation.x = Number(dx);
+			}
+
+			if (dy != undefined) {
+				this._rotation.y += Number(dy);
+			}
+			return this;
+		} else {
+			return this._rotation;
+		}
+
+
 	}
 	this.draw = function(ctx) {
 		// draw each vertex to get the first sphere skeleton
-		for (i = 0; i < this.numberOfVertices; i++) {
-			this.strokeSegment(i, ctx, width, height);
-		}
+		// for (i = 0, iz = this.vertices.length; i < iz; i++) {
+		// 	this.strokeSegment(i, ctx, width, height);
+		// }
 
 		// now walk through rings to draw the slices
 		for (i = 0; i < this.slices + 1; i++) {
 			for (var j = 0; j < this.rings + 1; j++) {
-				this.strokeSegment(this.vertices, i + (j * (this.slices + 1)), ctx, width, height);
+				this.strokeSegment(i + (j * (this.slices + 1)), ctx, this.width(), this.height());
 			}
 		}
 	}
@@ -101,10 +109,9 @@ function Sphere3D(radius) {
 	this.strokeSegment = function(index, ctx, width, height) {
 		var x, y;
 		var p = this.vertices[index];
-
-		rotateX(p, this.rotation.x);
-		rotateY(p, this.rotation.y);
-		rotateZ(p, this.rotation.z);
+		rotateX(p, this._rotation.x);
+		rotateY(p, this._rotation.y);
+		rotateZ(p, this._rotation.z);
 
 		x = projection(p.x, p.z, width / 2.0, 100.0, this.radius);
 		y = projection(p.y, p.z, height / 2.0, 100.0, this.radius);
@@ -114,7 +121,6 @@ function Sphere3D(radius) {
 			lastY = y;
 			return;
 		}
-
 		if (x >= 0 && x < width && y >= 0 && y < height) {
 			if (p.z < 0) {
 				ctx.strokeStyle = "gray";
@@ -135,7 +141,6 @@ Sphere3D.prototype = Object.create(Drawable.prototype);
 Sphere3D.prototype.constructor = Sphere3D;
 
 function rotateX(point, radians) {
-
 	var y = point.y;
 	point.y = (y * Math.cos(radians)) + (point.z * Math.sin(radians) * -1.0);
 	point.z = (y * Math.sin(radians)) + (point.z * Math.cos(radians));
