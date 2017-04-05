f658374c6cae3c2fee009361a257e44a24b2afd5
tito <tito@cefalo.com>
2017-04-05 11:32:41 +0600

    logo change

--- js/plugins/logo.js
+++ js/plugins/logo.js
@@ -1,48 +1,54 @@
 function Logo(input, output) {
 	Canvas.call(this, output);
 
+
 	var ctx = this._ctx;
 
 
 	//draw cricle
 	var zoom = 2;
-	var radius = 256;
+	var radius = 128;
 
 	var center = new Point(this.width / 2, this.height / 2);
+
 	ctx.save();
 
-	ctx.strokeStyle = '#666';
-	ctx.lineWidth = zoom * 1.5;
 
+	ctx.beginPath();
+	ctx.fillStyle = '#000';
+	ctx.lineWidth = 3;
 	ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
 	ctx.stroke();
-	var points = [];
-	//get positions
-	for (var i = 1; i <= 9; i++) {
-		var deg = (i * 40) - 90,
-			degRad = deg * Math.PI / 180;
-		points[i] = new Point(center.x + radius * Math.cos(degRad), center.y + radius * Math.sin(degRad));
-	}
-	//draw connection
-	var connecion = [1, 2, 4, 8, 7, 5];
+
 	ctx.beginPath();
-	ctx.strokeStyle = '#f00';
-	ctx.lineWidth = zoom * 1;
-	ctx.moveTo(points[connecion[0]].x, points[connecion[0]].y);
-	for (var i = 1, ln = connecion.length; i < ln; i++) {
-		ctx.lineTo(points[connecion[i]].x, points[connecion[i]].y);
-	}
-	ctx.closePath();
-	ctx.stroke();
-	//draw poller
+	ctx.fillStyle = '#000';
+	ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+	ctx.fill();
+
 	ctx.beginPath();
-	ctx.setLineDash([zoom * 1, zoom * 5]);
-	ctx.strokeStyle = '#00f';
-	ctx.lineWidth = zoom * 1;
-	ctx.moveTo(points[3].x, points[3].y);
-	ctx.lineTo(points[9].x, points[9].y);
-	ctx.lineTo(points[6].x, points[6].y);
-	ctx.stroke();
+	ctx.fillStyle = '#FFF';
+	ctx.arc(center.x, center.y, radius, Math.PI / 2, 1.5 * Math.PI);
+	ctx.fill();
+
+	ctx.beginPath();
+
+	ctx.fillStyle = '#000';
+	ctx.arc(center.x, center.y + radius / 2, radius / 2, Math.PI / 2, 1.5 * Math.PI);
+	ctx.fill();
+
+	ctx.beginPath();
+	ctx.fillStyle = '#FFF';
+	ctx.arc(center.x, center.y - radius / 2, radius / 2, Math.PI / 2, 1.5 * Math.PI, true);
+	ctx.fill();
+
+	ctx.beginPath();
+	ctx.arc(center.x, center.y + radius / 2, radius / 6, 0, 2 * Math.PI);
+	ctx.fill();
+
+	ctx.beginPath();
+	ctx.fillStyle = '#000';
+	ctx.arc(center.x, center.y - radius / 2, radius / 6, 0, 2 * Math.PI);
+	ctx.fill();
 
 	ctx.restore();
 	//save screen?
@@ -59,6 +65,8 @@ function Logo(input, output) {
 
 		window.open(tc.toDataURL("image/png"), '_blank');
 	}
+
+
 
 	// this.screenShoot(ctx, center.x - radius, center.y - radius, 2 * radius, 2 * radius);
 }
